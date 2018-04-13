1c15fa3e8dfa9a2586ae73ffd3a7e01c32cc2258
nerincon <nerincon1@gmail.com>
2018-04-12 23:44:44 -0500

    continue JS exercises

--- js_exercises_part1.js
+++ js_exercises_part1.js
@@ -19,4 +19,38 @@ function tipCalc(check_amount, service){
 }
 tipCalc(100, 'fair')
 
+//Print Numbers
+for(var i=1; i < 11; i++){
+    console.log(i)
+}
+//Print a Square
+function printSquare(num){
+    for(i = 0; i < 5; i++){
+        console.log('*'.repeat(num))
+    }
+}
+printSquare(5)
 
+//Print a box
+function printBox(w, h){
+    console.log('*'.repeat(w))
+    for(i=0; i < h-2; i++){
+        console.log('*' + ' '.repeat(w-2) + '*')
+    }
+    console.log('*'.repeat(w))
+}
+
+printBox(6,4)
+
+
+// Print a Banner
+function printBanner(context){
+    var w = context.length
+    var h = 3
+    console.log('*'.repeat(w+4))
+    for(i = 0; i < h-2; i++){
+        console.log('*' + ' ' + context + ' ' + '*')
+    }
+    console.log('*'.repeat(w+4))
+}
+printBanner('Welcome to DigitalCrafts')
