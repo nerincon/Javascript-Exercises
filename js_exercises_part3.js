ed10673bee809331ee02ec61ab5aeff9c8c27b5d
nerincon <nerincon1@gmail.com>
2018-04-15 14:56:24 -0500

    uncomment JS exercises part3

--- js_exercises_part3.js
+++ js_exercises_part3.js
@@ -1,49 +1,49 @@
-// // Function Exercises
+// Function Exercises
 
-// // Even Numbers
-// var A = []
-// function evenNumbers(arr){
-//     for(var i = 0; i < arr.length; i++){
-//         if(arr[i] % 2 === 0){
-//             A.push(arr[i])
-//         }
-//     }
-// }
-// evenNumbers([1,2,3,4,5,6,7,8,9,10])
-// console.log(A)
+// Even Numbers
+var A = []
+function evenNumbers(arr){
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] % 2 === 0){
+            A.push(arr[i])
+        }
+    }
+}
+evenNumbers([1,2,3,4,5,6,7,8,9,10])
+console.log(A)
 
-// // Square the Numbers
-// var A2 = []
-// function evenNumbers(arr){
-//     for(var i = 0; i < arr.length; i++){
-//         A2.push(arr[i] * arr[i])
-//     }
-// }
-// evenNumbers([1,2,3,4,5,6,7,8,9,10])
-// console.log(A2)
+// Square the Numbers
+var A2 = []
+function evenNumbers(arr){
+    for(var i = 0; i < arr.length; i++){
+        A2.push(arr[i] * arr[i])
+    }
+}
+evenNumbers([1,2,3,4,5,6,7,8,9,10])
+console.log(A2)
 
-// //OR
-// var a = [1, 2, 3, 4];
-// function times2 (n) {
-//   return n * 2;
-// }
-// var doubled = a.map(times2);
-// console.log(doubled)
+//OR
+var a = [1, 2, 3, 4];
+function times2 (n) {
+  return n * 2;
+}
+var doubled = a.map(times2);
+console.log(doubled)
 
-// // Cities
-// var cities = [
-//     { name: 'Los Angeles', temperature: 60.0},
-//     { name: 'Atlanta', temperature: 52.0 },
-//     { name: 'Detroit', temperature: 48.0 },
-//     { name: 'New York', temperature: 80.0 }
-//   ];
+// Cities
+var cities = [
+    { name: 'Los Angeles', temperature: 60.0},
+    { name: 'Atlanta', temperature: 52.0 },
+    { name: 'Detroit', temperature: 48.0 },
+    { name: 'New York', temperature: 80.0 }
+  ];
 
-//   function city(d){
-//       for(var i = 0; i < d.length; i++){
-//           if(d[i]['temperature'] < 70){
-//               console.log(d[i])
-//           }
-//       }
-//   }
-//   city(cities)
+  function city(d){
+      for(var i = 0; i < d.length; i++){
+          if(d[i]['temperature'] < 70){
+              console.log(d[i])
+          }
+      }
+  }
+  city(cities)
 
