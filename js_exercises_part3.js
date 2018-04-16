d3d79c6bb31814211ca6ff520f2e3bf5871b2819
nerincon <nerincon1@gmail.com>
2018-04-15 20:41:08 -0500

    finish JS exercises part3

--- js_exercises_part3.js
+++ js_exercises_part3.js
@@ -47,3 +47,87 @@ var cities = [
   }
   city(cities)
 
+// Good Job
+var people = ['Dom','Lyn','Kirk','Autumn','Trista','Jesslyn','Kevin','John','Eli','Juan','Robert','Keyur',
+    'Jason','Che','Ben'];
+
+function goodJob(p){
+    for(var i = 0; i < p.length; i++){
+        console.log("Good Job " + p[i] +"!")
+    }
+}
+goodJob(people)
+
+// Sort an array
+var people = ['Dom','Lyn','Kirk','Autumn','Trista','Jesslyn','Kevin','John','Eli','Juan','Robert','Keyur',
+    'Jason','Che','Ben'];
+
+people.sort(function (x, y) {
+    if (x > y) { return 1; }
+    else if (x < y) { return -1; }
+    return 0;
+    });
+console.log(people)
+
+// Sort an array, 2 :: By name length
+var people = ['Dom','Lyn','Kirk','Autumn','Trista','Jesslyn','Kevin','John','Eli','Juan','Robert','Keyur',
+    'Jason','Che','Ben'];
+
+people.sort(function (x, y) {
+    if (x.length > y.length) { return 1; }
+    else if (x.length < y.length) { return -1; }
+    return 0;
+    });
+console.log(people)
+
+// 3 times
+
+function call3Times(fun) {
+    for(var i = 0; i < 3; i++){
+        console.log(fun)
+    }
+  }
+
+call3Times('Hello World!')
+
+// n times
+
+function callNTimes(n, fun) {
+    for(var i = 0; i < n; i++){
+        console.log(fun)
+    }
+  }
+
+callNTimes(5,'Hello World!')
+
+// Sum an array
+
+function sum(arr){
+    var output = arr.reduce((acc, val) => acc + val)
+    console.log(output)
+}
+sum([1,2,3])
+
+
+// Acronym
+function acronym(arr){
+    var output = arr.reduce((acc,val) => {
+        return acc.concat(val[0]);
+    }, '');
+    console.log(output.toUpperCase())
+}
+
+acronym(['very', 'important', 'person'])
+acronym(['national', 'aeronautics', 'space', 'administration'])
+
+
+// FOR EACH
+var names = [
+    { first_name: 'Bob', last_name: 'Vance' },
+    { first_name:'Alice', last_name: 'Wonderland' },
+    { first_name:'Joe', last_name: 'Doe' }
+  ];
+  
+names.forEach(function(p) {
+    console.log('Hello, ' + p.first_name + ' ' + p.last_name + '!');
+});
