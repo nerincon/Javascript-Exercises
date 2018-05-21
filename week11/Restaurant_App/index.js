fac9d523dc81c095ab110566ea52f859a83c128a
nerincon <nerincon1@gmail.com>
2018-05-20 21:35:40 -0500

    add multi-query and modify rest html

--- week11/Restaurant_App/index.js
+++ week11/Restaurant_App/index.js
@@ -34,16 +34,26 @@ app.get('/search', function (req, res, next){
     .catch(next)
 });
 
+
+
 app.get('/restaurant/:id', function (req, res, next){
     var searchTerm = req.query.searchTerm;
-    let query = "SELECT * FROM restaurant WHERE restaurant.id = ${id}";
     let id = req.params.id;
-    db.any(query, {searchTerm, id})
-    .then(function(resultsArray){
-        res.render('restaurant', {results: resultsArray});
+    let query = 'SELECT restaurant.name AS restaurant_name, restaurant.address AS address,\
+    restaurant.category AS category FROM restaurant WHERE restaurant.id = ${id};\
+    SELECT reviewer.name AS reviewer_name, review.title AS review_title,\
+    review.review AS review, review.stars AS stars FROM review\
+    JOIN restaurant ON restaurant.id = restaurant_id\
+    JOIN reviewer ON reviewer.id = reviewer_id WHERE restaurant.id = ${id}'
+    db.multi(query, {searchTerm, id})
+    .then(data => {
+        // data[0] = result from the first query;
+        // data[1] = result from the second query;
+        res.render('restaurant', {data: data[0], data2: data[1]});
     })
     .catch(next)
 });
+
 
 app.listen(8000, function () {
     console.log('Listening on port 8000');
