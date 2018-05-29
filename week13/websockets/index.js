72f4380cfd333fe8951ef4d8cc53550df7070c6d
nerincon <nerincon1@gmail.com>
2018-05-28 22:01:22 -0500

    continue chat app

--- week13/websockets/index.js
+++ week13/websockets/index.js
@@ -3,6 +3,8 @@ const nunjucks = require('nunjucks');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var users = [];
+var connections = [];
 
 
 nunjucks.configure('views', {
@@ -21,16 +23,36 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    console.log('message: ' + msg);
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
+
+  //Send Message
+  socket.on('send message', function(data){
+    console.log('message: ' + data);
+    io.emit('new message', {msg: data, user: socket.username});
   });
+
+  //New User
+  socket.on('new user', function(data, callback){
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+  });
+
+  //Disconnect
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    if(!socket.username){return}
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected: %s sockets connected', connections.length);
   });
 });
 
+function updateUsernames() {
+  io.emit('get users', users);
+}
 
 http.listen(8080, function () {
   console.log('Listening on port 8080');
